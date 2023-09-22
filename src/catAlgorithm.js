37beaf893bb16afd724e6cba0acec850dda1c009
Bilal <ayachi@DESKTOP-U0SA8PR>
2023-09-22 16:51:07 +0200

    algor implemented

--- src/catAlgorithm.js
+++ src/catAlgorithm.js
@@ -1,10 +1,17 @@
 function removeAllChildNodes() {
     while (document.getElementById("output").firstChild) {
         console.log(document.getElementById("output").removeChild(document.getElementById("output").firstChild));
-        // console.log(document.getElementById("output").firstChild);     
         document.getElementById("output").removeChild(document.getElementById("output").firstChild)
     }
 }
+
+function printCats (amount) {
+    let output = "";
+    for (let i = 0; i < amount; i++) {
+        output += "🐱 "; 
+    }
+    return output;
+};
 
 function generateCats (amountOfIterations) {
     console.log("clicjk");
@@ -14,9 +21,26 @@ function generateCats (amountOfIterations) {
     let child = document.createElement("p");
     child.innerText = "🐱";
     document.getElementById("output").appendChild(child);
-
-    for (let i = 0; i < amountOfIterations; i++) {
-
+    for (let i = 1; i < amountOfIterations; i++) {
+        if (amountOfCats % 2 != 0 && counter == 0) {
+            amountOfCats = amountOfCats + 5;
+            counter = 1;
+            let childN = document.createElement("p");
+            childN.innerText = printCats(amountOfCats);
+            document.getElementById("output").appendChild(childN);
+            debugger;
+        } else if (amountOfCats % 2 != 0 && counter == 1) {
+            amountOfCats = amountOfCats + 13;
+            counter = 0;
+            let childN = document.createElement("p");
+            childN.innerText = printCats(amountOfCats);
+            document.getElementById("output").appendChild(childN);
+        } else {
+            amountOfCats = amountOfCats / 2;
+            let childN = document.createElement("p");
+            childN.innerText = printCats(amountOfCats);
+            document.getElementById("output").appendChild(childN);
+        };
     };
     console.log(amountOfCats);
 };
