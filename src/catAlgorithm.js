198f037d7268ad734c0daca5b18204a1d39f0f07
Bilal <ayachi@DESKTOP-U0SA8PR>
2023-09-22 16:34:27 +0200

    clear div code

--- src/catAlgorithm.js
+++ src/catAlgorithm.js
@@ -1,5 +1,22 @@
+function removeAllChildNodes() {
+    while (document.getElementById("output").firstChild) {
+        console.log(document.getElementById("output").removeChild(document.getElementById("output").firstChild));
+        // console.log(document.getElementById("output").firstChild);     
+        document.getElementById("output").removeChild(document.getElementById("output").firstChild)
+    }
+}
 
+function generateCats (amountOfIterations) {
+    console.log("clicjk");
+    removeAllChildNodes();
+    let amountOfCats = 1;
+    let counter = 0;
+    let child = document.createElement("p");
+    child.innerText = "🐱";
+    document.getElementById("output").appendChild(child);
 
-function generateCats (amountOfCats) {
+    for (let i = 0; i < amountOfIterations; i++) {
+
+    };
     console.log(amountOfCats);
 };
